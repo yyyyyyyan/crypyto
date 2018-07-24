7a9a183f92db35e3cdba6494fcc5012bf3655d6e
Yan <yanorestes@hotmail.com>
2018-07-24 15:40:21 -0300

    Now using dict.get()

--- ciphers.py
+++ ciphers.py
@@ -11,7 +11,7 @@ class PolybiusSquare:
 			cipher = cipher.lower()
 			match = re.search(r'(\d+)x(\d+)#(?:\d+-\d+;?)+', cipher)
 			if not match:
-				raise ValueError('Cipher doesn\'t match the Polybius Square pattern.')
+				raise ValueError('Can\'t find square size within the cipher, please specify with width and height parameters')
 
 		self.width = width if width else int(match.group(1))
 		self.height = height if height else int(match.group(2))
@@ -41,21 +41,22 @@ class PolybiusSquare:
 		for letter in self.abc:
 			for pos in self.abc_to_pos[letter]:
 				self.pos_to_abc[pos] = letter
+		self.pos_to_abc['0-0'] = '�'
 
 	def encrypt(self, text):
 		text = unidecode(text).upper()
 		text = text.replace('J', 'I') if len(self.abc) == 25 else text
 		cipher = '{}x{}#'.format(self.width, self.height)
-		positions = [random.choice(self.abc_to_pos[letter]) for letter in text]
+		positions = [random.choice(self.abc_to_pos.get(letter, ['0-0'])) for letter in text]
 		cipher += ';'.join(positions)
 		return cipher
 
 	def decrypt(self, cipher):
 		cipher = cipher.lower()
-		match = re.search(r'\d+x\d+#((?:\d+-\d+;?)+)', cipher)
+		match = re.search(r'(?:\d+x\d+#)?((?:\d+-\d+;?)+)', cipher)
 		if match:
 			positions = match.group(1).split(';')
-			text = ''.join([self.pos_to_abc[pos] for pos in positions])
+			text = ''.join([self.pos_to_abc.get(pos, '0-0') for pos in positions])
 			return text
 		else:
 			raise ValueError('Cipher doesn\'t match the Polybius Square pattern.')
