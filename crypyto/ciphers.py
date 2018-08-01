e01962475660d34e84c9e0b0819c64dc7f7f7a66
Yan <yanorestes@hotmail.com>
2018-08-01 16:23:07 -0300

    Refactored (now use properties)

--- crypyto/ciphers.py
+++ crypyto/ciphers.py
@@ -5,29 +5,29 @@ from math import gcd
 from unidecode import unidecode
 
 class PolybiusSquare:
-    def __init__(self, width=0, height=0, cipher=None, ij=True):
-        if not ((width and height) or cipher):
-            raise ValueError('Either choose the size of the square (width/height) or the encrypted cypher.')
-
-        if cipher:
-            cipher = cipher.lower()
-            match = re.search(r'(\d+)x(\d+)#(?:\d+-\d+;?)+', cipher)
-            if not match:
-                raise ValueError('Can\'t find square size within the cipher, please specify with width and height parameters')
-
-        self.width = width if width else int(match.group(1))
-        self.height = height if height else int(match.group(2))
-
-        self.abc = string.ascii_uppercase
+    def __init__(self, width, height, abc=string.ascii_uppercase, ij=True):
         self.abc = self.abc.replace('J', '') if ij else self.abc
-
-        if width * height < len(self.abc):
-            raise ValueError('This square is not large enough to comprehend the whole alphabet.\nPlease increase width or height.')
-
+        self.width = width
+        self.height = height
         self.mount_square()
 
-        if cipher:
-            print(self.decrypt(match.group(0)))
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value < 1:
+            raise ValueError('This square is not large enough to comprehend the whole alphabet.\nPlease increase width or height.')
+
+    @property
+    def height(self):
+        return self._width
+
+    @height.setter
+    def height(self, value):
+        if value * self.width < len(self.abc):
+            raise ValueError('This square is not large enough to comprehend the whole alphabet.\nPlease increase width or height.')
 
     def mount_square(self):
         self.square_area = self.width * self.height
@@ -114,13 +114,20 @@ ROT13 = Caesar(key=13)
 
 class Affine:
     def __init__(self, a, b, abc=string.ascii_uppercase):
-        if gcd(a, len(abc)) != 1:
-            raise ValueError('Parameter a must be coprime to {}'.format(len(abc)))
+        self.abc = abc
         self.a = a
         self.b = b
-        self.abc = abc
         self.pos_to_abc = dict(enumerate(abc))
         self.abc_to_pos = {v:k for k, v in self.pos_to_abc.items()}
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, value):
+        if gcd(value, len(self.abc)) != 1:
+            raise ValueError('Parameter a must be coprime to {}'.format(len(self.abc)))
 
     def encrypt(self, text):
         text = unidecode(text).upper()
@@ -147,13 +154,21 @@ class Affine:
 
 class RailFence:
     def __init__(self, n_rails, only_alnum=False, direction='D'):
-        if direction not in ['D','U']:
-            raise ValueError('direction must be (U)p or (D)own')
-
+        self.direction = direction
         self.n_rails = n_rails
         self.only_alnum = only_alnum
         self.not_alnum_pattern = re.compile(r'[\W_]+', re.UNICODE)
         self.direction = direction
+
+    @property
+    def direction(self):
+        return self._direction
+    
+    @direction.setter
+    def direction(self, value):
+        if value not in ['D', 'U']:
+            raise ValueError('direction must be (U)p or (D)own')
+        self._direction = value
 
     def encrypt(self, text):
         text = self.not_alnum_pattern.sub('', text) if self.only_alnum else text
