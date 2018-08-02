a931657187a37e4bf90de52bae527b7e73586fda
Yan <yanorestes@hotmail.com>
2018-08-02 18:28:11 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('long_description.rst') as file:
 
 setup(
     name='crypyto',
-    version='0.1.0',
+    version='0.1.1',
     author='Yan Orestes',
     author_email='yan.orestes@alura.com.br',
     packages=['crypyto'],
     description='crypyto is a Python package that provides simple usage of cryptography tools and ciphers on your programs.',
     long_description=long_description,
     url='https://github.com/yanorestes/crypyto',
-    download_url='https://github.com/yanorestes/crypyto/archive/0.1.0.zip',
+    download_url='https://github.com/yanorestes/crypyto/archive/0.1.1.zip',
     license='MIT',
     keywords='crypto cryptography cipher',
     classifiers=[
